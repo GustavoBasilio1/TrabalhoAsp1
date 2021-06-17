3ed1191d01bd78a581edfa3d32fb39022b0c4f7b
GustavoBasilio1 <gustavo.cancian@etec.sp.gov.br>
2021-06-17 14:00:06 -0300

    Add project files.

---
+++ AtividadeAsp1/Controllers/ProdutosController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AtividadeAsp1.Models;
+
+
+
+namespace AtividadeAsp1.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public ActionResult Index()
+        {
+            var produtos = new Produtos(); //Criando o objeto para classe
+            return View(produtos); //Retornando para a view os dados da classe
+        }
+        [HttpPost]
+        //Criar uma nova index, clicar com o botão direto do mouse
+        //AddView manter o nome index e substituir
+
+
+
+        public ActionResult Index(Produtos produtos)
+        {
+            if (ModelState.IsValid)//Validando o estado, ou seja, verificando a requisição
+            {
+                return View("Result", produtos); //Retorna para a view resultado
+            }
+            return View(produtos);
+        }
+
+
+
+        public ActionResult Result(Produtos produtos) //Criando a ação para a view resultado
+        {
+            return Result(produtos);
+        }
+
+
+
+
+        public ActionResult LoginUnico(string codigo)
+        {
+            var bdProdutos = new Collection<string>
+            {
+            "4",
+            "9"
+            };
+            return Json(bdProdutos.All(x => x.ToLower() != codigo.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+
+
+
+    }
+}
---
+++ AtividadeAsp1/Models/Produtos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web;
+
+
+
+namespace AtividadeAsp1.Models
+{
+
+
+
+    public class Produtos
+    {
+        [Required(ErrorMessage = "O campo Código é obrigatório")]
+        [Remote("LoginUnico", "produtos", ErrorMessage = "Login existente.")]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Descrição é obrigatório")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Insira de 5 a 50 caracteres!")]
+        public string Descricao { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Valor inválido")]
+        public string Peso { get; set; }
+
+        [Required(ErrorMessage = "O campo Categoria é obrigatório")]
+        public string Categoria { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Valor inválido")]
+        [Required(ErrorMessage = "O campo ValorUnit é obrigatório")]
+        public string ValorUnitario { get; set; }
+
+
+
+    }
+}
+
